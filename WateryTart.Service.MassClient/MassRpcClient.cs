1049aab310d4689330fd06b0bf4e44ac1d8ab3f2
Paul Jenkins <paul@thewoodknight.com>
2026-02-08 16:20:54 +1100

    very basic rpc client added, just in case it becomes relevant

---
+++ WateryTart.Service.MassClient/MassRpcClient.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using WateryTart.Service.MassClient.Messages;
+using WateryTart.Service.MassClient.Models.Auth;
+
+namespace WateryTart.Service.MassClient
+{
+    public class MassRpcClient
+    {
+        private readonly IMassCredentials _credentials;
+        private readonly string _baseUrl;
+        private HttpClient client;
+        public MassRpcClient(IMassCredentials credentials, string baseUrl = "http://10.0.1.20:8095/api")
+        {
+            _credentials = credentials;
+            _baseUrl = baseUrl;
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", credentials.Token);
+
+
+           // Send<List<Player>>(new Message(Commands.PlayersAll));
+        }
+
+        public async Task<T?> Send<T>(MessageBase message)
+        {
+            //Build request
+            var request = new HttpRequestMessage(HttpMethod.Post, _baseUrl);
+            request.Content = new StringContent(message.ToJson());
+
+            //Send request
+            var response = await client.SendAsync(request);
+
+            //Convert response to T 
+            var responseBody = await response.Content.ReadAsStringAsync();
+            T responseProper = JsonSerializer.Deserialize<T>(responseBody);
+
+            return responseProper;
+        }
+    }
+}
---
+++ WateryTart.Service.MassClient/RPCExtensions/RpcClientExtensions.cs
@@ -0,0 +1,61 @@
+﻿using WateryTart.Service.MassClient.Messages;
+using WateryTart.Service.MassClient.Models;
+using WateryTart.Service.MassClient.Responses;
+
+namespace WateryTart.Service.MassClient;
+
+public static partial class MassClientExtensions
+{
+    extension(MassRpcClient c)
+    {
+        public async Task<List<Player>?> PlayersAllAsync()
+        {
+            return await c.Send<List<Player>>(JustCommand(Commands.PlayersAll));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerNextAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerNext, playerId, "player_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerPlayAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerPlay, playerId, "player_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerPlayPauseAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerPlayPause, playerId, "player_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerPreviousAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerPrevious, playerId, "player_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerQueuesAllAsync()
+        {
+            return await c.Send<List<PlayerQueue>>(JustCommand(Commands.PlayerQueuesAll));
+        }
+
+        public async Task<PlayerQueue?> PlayerActiveQueueAsync(string id)
+        {
+            return await c.Send<PlayerQueue>(JustId(Commands.PlayerActiveQueue, id, "player_id"));
+        }
+        
+        public async Task<List<PlayerQueue>?> PlayerQueueItemsAsync(string id)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerQueueItems, id, "queue_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerGroupVolumeUpAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerGroupVolumeUp, playerId, "player_id"));
+        }
+
+        public async Task<List<PlayerQueue>?> PlayerGroupVolumeDownAsync(string playerId)
+        {
+            return await c.Send<List<PlayerQueue>>(JustId(Commands.PlayerGroupVolumeDown, playerId, "player_id"));
+        }
+    }
+}
