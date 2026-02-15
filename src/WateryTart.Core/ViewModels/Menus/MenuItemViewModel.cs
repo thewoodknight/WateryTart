e65c7feb67a486d33b2534fd76bd3a03d6f92050
Paul Jenkins <paul@thewoodknight.com>
2026-02-15 20:32:56 +1100

    Fix: Null Menu commands won't cause the menu to crash or close

--- src/WateryTart.Core/ViewModels/Menus/MenuItemViewModel.cs
+++ src/WateryTart.Core/ViewModels/Menus/MenuItemViewModel.cs
@@ -19,13 +19,15 @@ public partial class MenuItemViewModel : ReactiveObject, ISmallViewModelBase, IM
         Indented = indented;
         _title = title;
         if (icon != null)
-        _icon = (MaterialIconKind)icon;
+            _icon = (MaterialIconKind)icon;
         _clickedCommand = clickedCommand;
     }
 
     [GenerateTypedAction]
     public void MenuItemClicked()
     {
+        if (_clickedCommand == null)
+            return;
         _clickedCommand.Execute(null);
         MessageBus.Current.SendMessage(new CloseMenuMessage());
     }
