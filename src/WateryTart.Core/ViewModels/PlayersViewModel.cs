95ed02e847efc62ba22c784cbe3aec6cb25ebe39
Paul Jenkins <paul@thewoodknight.com>
2026-02-25 15:14:53 +1100

    feat(Visuals): Players tray now looks significantly nicer.

--- src/WateryTart.Core/ViewModels/PlayersViewModel.cs
+++ src/WateryTart.Core/ViewModels/PlayersViewModel.cs
@@ -20,15 +20,16 @@ using WateryTart.MusicAssistant;
 using WateryTart.MusicAssistant.Models;
 using WateryTart.MusicAssistant.Models.Enums;
 using Xaml.Behaviors.SourceGenerators;
+using System.Windows.Input;
 namespace WateryTart.Core.ViewModels;
 
 public partial class PlayersViewModel : ReactiveObject, IViewModelBase
 {
-    [Reactive] public partial PlayersService _playersService { get; set; }
+    [Reactive] public partial PlayersService PlayersService { get; set; }
     public string? UrlPathSegment { get; } = "players";
     public IScreen HostScreen { get; }
-    public string Title => "Players";
-    public bool ShowMiniPlayer => true;
+    public string Title => "Connect To A Device";
+    public bool ShowMiniPlayer => false;
     public bool ShowNavigation => true;
     [Reactive] public partial ObservableCollection<Player> Players { get; set; }
     [Reactive] public partial bool IsLoading { get; set; } = false;
@@ -37,7 +38,7 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
         get => field;
         set
         {
-            _playersService.SelectedPlayer = value;
+            PlayersService.SelectedPlayer = value;
             this.RaiseAndSetIfChanged(ref field, value);
         }
     }
@@ -45,6 +46,22 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
     [Reactive] public partial RelayCommand<Player> PlayerAltCommand { get; set; }
     [Reactive] public partial RelayCommand<Player> PlayerTogglePlayPauseCommand { get; set; }
     [Reactive] public partial ColourService ColourService { get; set; }
+
+    public ICommand ToggleFavoriteCommand { get; set; }
+
+    public ICommand ToggleShuffleCommand { get; set; }
+
+    public ICommand CycleRepeatCommand { get; set; }
+
+    public ICommand PlayingAltMenuCommand { get; set; }
+    public ICommand PlayPreviousCommand { get; set; }
+    public ICommand PlayerRepeatTrack { get; set; }
+    public ICommand PlayerRepeatQueue { get; set; }
+    public ICommand PlayerRepeatOff { get; set; }
+
+    public ICommand PlayerNextCommand { get; set; }
+    public ICommand PlayerPlayPauseCommand { get; set; }
+
     private double _volume;
     private double _pendingVolume;
     private CancellationTokenSource? _volumeCts;
@@ -58,17 +75,17 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
 
     public PlayersViewModel(MusicAssistantClient massClient, IScreen screen, PlayersService playersService, ColourService colourService)
     {
-        _playersService = playersService;
+        PlayersService = playersService;
         HostScreen = screen;
         Players = playersService.Players;
         SelectedPlayer = playersService.SelectedPlayer;
         ColourService = colourService;
 
         // Initialize from selected player if available
-        Volume = _playersService?.SelectedPlayer?.VolumeLevel ?? 0;
+        Volume = PlayersService?.SelectedPlayer?.VolumeLevel ?? 0;
 
         // Keep VM Volume in sync with server updates, suppress round-trip
-        this.WhenAnyValue(x => x._playersService.SelectedPlayer.VolumeLevel)
+        this.WhenAnyValue(x => x.PlayersService.SelectedPlayer.VolumeLevel)
             .ObserveOn(RxApp.MainThreadScheduler)
             .Subscribe(serverVol =>
             {
@@ -86,7 +103,7 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
         PlayerTogglePlayPauseCommand = new RelayCommand<Player>(p =>
         {
 #pragma warning disable CS4014
-            _playersService.PlayerPlayPause(p);
+            PlayersService.PlayerPlayPause(p);
 #pragma warning restore CS4014
         });
         PlayerAltCommand = new RelayCommand<Player>(p =>
@@ -94,31 +111,31 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
             var menu = new MenuViewModel();
 #pragma warning disable CS4014
             if (p.PlaybackState == MusicAssistant.Models.Enums.PlaybackState.Playing)
-                menu.AddMenuItem(new MenuItemViewModel("Stop Playback", PackIconMaterialKind.Stop, new RelayCommand(() => _playersService.PlayerPlayPause(p))));
+                menu.AddMenuItem(new MenuItemViewModel("Stop Playback", PackIconMaterialKind.Stop, new RelayCommand(() => PlayersService.PlayerPlayPause(p))));
 
-            var queue = _playersService.Queues.FirstOrDefault(q => q.QueueId == p.ActiveSource);
+            var queue = PlayersService.Queues.FirstOrDefault(q => q.QueueId == p.ActiveSource);
             if (queue != null)
             {
                 if (!queue.ShuffleEnabled)
-                    menu.AddMenuItem(new MenuItemViewModel("Enable shuffle", PackIconMaterialKind.Shuffle, new RelayCommand(() => _playersService.PlayerShuffle(p))));
+                    menu.AddMenuItem(new MenuItemViewModel("Enable shuffle", PackIconMaterialKind.Shuffle, new RelayCommand(() => PlayersService.PlayerShuffle(p))));
                 else
-                    menu.AddMenuItem(new MenuItemViewModel("Disable shuffle", PackIconMaterialKind.Shuffle, new RelayCommand(() => _playersService.PlayerShuffle(p, false))));
+                    menu.AddMenuItem(new MenuItemViewModel("Disable shuffle", PackIconMaterialKind.Shuffle, new RelayCommand(() => PlayersService.PlayerShuffle(p, false))));
                 var items = new List<MenuItemViewModel>()
                 { 
                     //new MenuItemViewModel("Syncronize with another player", PackIconMaterialKind.Link, null),
                     new MenuItemViewModel("Repeat Mode", PackIconMaterialKind.Repeat, null),
-                    new MenuItemViewModel("Repeat Off", PackIconMaterialKind.RepeatOff, new RelayCommand(() => _playersService.PlayerSetRepeatMode(RepeatMode.Off, p)), true),
-                    new MenuItemViewModel("Repeat Entire Queue", PackIconMaterialKind.RepeatVariant, new RelayCommand(() => _playersService.PlayerSetRepeatMode(RepeatMode.All, p)), true),
-                    new MenuItemViewModel("Repeat Single Track", PackIconMaterialKind.Repeat, new RelayCommand(() => _playersService.PlayerSetRepeatMode(RepeatMode.One, p)), true),
+                    new MenuItemViewModel("Repeat Off", PackIconMaterialKind.RepeatOff, new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.Off, p)), true),
+                    new MenuItemViewModel("Repeat Entire Queue", PackIconMaterialKind.RepeatVariant, new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.All, p)), true),
+                    new MenuItemViewModel("Repeat Single Track", PackIconMaterialKind.Repeat, new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.One, p)), true),
                     //  new MenuItemViewModel("Transfer queue", PackIconMaterialKind.Transfer, null),
-                    new MenuItemViewModel("Clear queue", PackIconMaterialKind.Cancel, new RelayCommand(() => _playersService.PlayerClearQueue(p)))
+                    new MenuItemViewModel("Clear queue", PackIconMaterialKind.Cancel, new RelayCommand(() => PlayersService.PlayerClearQueue(p)))
                     // new MenuItemViewModel("Select source", PackIconMaterialKind.Import, null)
                 };
 
                 if (!queue.DontStopTheMusicEnabled)
-                    menu.AddMenuItem(new MenuItemViewModel("Enable 'Don't stop the music'", PackIconMaterialKind.Infinity, new RelayCommand(() => _playersService.PlayerDontStopTheMusic(p))));
+                    menu.AddMenuItem(new MenuItemViewModel("Enable 'Don't stop the music'", PackIconMaterialKind.Infinity, new RelayCommand(() => PlayersService.PlayerDontStopTheMusic(p))));
                 else
-                    menu.AddMenuItem(new MenuItemViewModel("Disable 'Don't stop the music'", PackIconMaterialKind.Infinity, new RelayCommand(() => _playersService.PlayerDontStopTheMusic(p, false))));
+                    menu.AddMenuItem(new MenuItemViewModel("Disable 'Don't stop the music'", PackIconMaterialKind.Infinity, new RelayCommand(() => PlayersService.PlayerDontStopTheMusic(p, false))));
 
                 menu.AddMenuItem(items);
             }
@@ -131,6 +148,32 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
         });
 
 
+
+#pragma warning disable CS4014
+        CycleRepeatCommand = new RelayCommand(() =>
+        {
+            var mode = PlayersService.SelectedQueue.RepeatMode;
+            switch (mode)
+            {
+                case RepeatMode.Off:
+                    PlayersService.PlayerSetRepeatMode(RepeatMode.All);
+                    break;
+                case RepeatMode.All:
+                    PlayersService.PlayerSetRepeatMode(RepeatMode.One);
+                    break;
+                case RepeatMode.One:
+                    PlayersService.PlayerSetRepeatMode(RepeatMode.Off);
+                    break;
+            }
+        });
+        ToggleShuffleCommand = new RelayCommand(() => PlayersService.PlayerShuffle(null, !PlayersService.SelectedQueue.ShuffleEnabled));
+        PlayerRepeatQueue = new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.All));
+        PlayerRepeatOff = new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.Off));
+        PlayerRepeatTrack = new RelayCommand(() => PlayersService.PlayerSetRepeatMode(RepeatMode.One));
+        PlayPreviousCommand = new RelayCommand(() => PlayersService.PlayerPrevious());
+        PlayerNextCommand = new RelayCommand(() => PlayersService.PlayerNext());
+        PlayerPlayPauseCommand = new RelayCommand(() => PlayersService.PlayerPlayPause());
+#pragma warning restore CS4014
     }
 
     // Add a slider change handler (or use the existing one) that debounces and calls PlayersService
@@ -164,7 +207,7 @@ public partial class PlayersViewModel : ReactiveObject, IViewModelBase
                 try
                 {
                     await Task.Delay(200, ct).ConfigureAwait(false);
-                    await _playersService.PlayerVolume((int)pending, player).ConfigureAwait(false);
+                    await PlayersService.PlayerVolume((int)pending, player).ConfigureAwait(false);
                 }
                 catch (System.OperationCanceledException) { }
                 catch (Exception ex)
